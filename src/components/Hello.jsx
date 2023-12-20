58a4dae1d415f1d88000ec483c276dcedb7a161d
maximoortelli <maximoortelli@hotmail.com>
2023-12-20 00:02:12 -0300

    Add all files
    Add all files

---
+++ src/components/Hello.jsx
@@ -0,0 +1,14 @@
+
+
+const Hello = (props) => {
+    console.log(props)
+  return (
+    <div>
+        <p>
+             Hello {props.name}, you are {props.age} years old
+        </p>
+    </div>
+  )
+}
+
+export default Hello
